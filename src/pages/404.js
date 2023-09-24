4b4011911e5616bc4b5ee8614a0f5f290a985e3c
grand3680 <kirill.burtsev07@gmail.com>
2023-09-24 22:46:38 +0300

    add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
-import variables from '@/styles/variables.module.scss';
+import style from '@/styles/404.module.scss';
 import Head from 'next/head';
+
 
 export default function Custom404() {
     return ( 
@@ -7,8 +8,13 @@ export default function Custom404() {
             <Head>
                 <title>404</title>
             </Head>
-            <h1 style={{ color: variables.primaryColor }}>404</h1>
-            <p>something wrong</p>
+
+            <div className={style.Error_404}>
+                <h1 className={style.Err_Title}>404</h1>
+                <h2 className={style.Err_text}>
+                    oops, someThing wrong, maybe you write not correct adress, or domain not exist
+                </h2>
+            </div>
         </>
     )
 }
