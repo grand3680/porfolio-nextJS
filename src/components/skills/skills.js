8324b30e70149607dcd79ad540efb4ededd29a46
grand3680 <kirill.burtsev07@gmail.com>
2023-09-25 00:52:39 +0300

    add skill page

---
+++ src/components/skills/skills.js
@@ -0,0 +1,46 @@
+const someTestSkill = [
+    {
+        category: 'Collectors',
+        skills: ['webpack']
+    },
+    {
+        category: 'CSS',
+        skills: ['Positioning', 'Visibility', 'Animations', 'Dimensions']
+    },
+    {
+        category: 'Database',
+        skills: ['mySql', 'regular sql']
+    },
+    {
+        category: 'development environment',
+        skills: ['VsCode', 'pySharm']
+    },
+    {
+        category: 'Frameworks',
+        skills: ['Django', 'A little Flask', 'React.js && Babel.js']
+    },
+    {
+        category: 'Graphics (not much)',
+        skills: ['Figma', 'Photoshop']
+    },
+    {
+        category: 'HTML',
+        skills: ['responsive blocks', '@media tegs', 'meta and viewport']
+    },
+    {
+        category: 'internet',
+        skills: ['protocols', 'Xostings', 'Domains names']
+    },
+    {
+        category: 'JavaScript',
+        skills: ['modules', 'a little NodeJs', 'canvas and animation']
+    },
+    {
+        category: 'learn sources',
+        skills: ['code-wars 3kye', 'English server on Discord']
+    }
+
+]
+
+
+export default someTestSkill;
---
+++ src/components/skills/skillses.js
@@ -0,0 +1,25 @@
+// import styles from '../styles/Main.module.main.scss'
+import styles from './styles/skills.module.scss';
+import someTestSkill from './skills';
+
+const Skilses = () => (
+  <>
+    <ul className={styles.skills}>
+        {someTestSkill.map(({category, skills}) => 
+            <li className={styles.skills_li}>
+                <h2 className={styles.skills_category}>{category}</h2>
+                <ul className={styles.skills_ul}>
+                    {skills ? 
+                        skills.map(skill => (
+                            <li className={styles.skills_description}>{skill}</li>
+                        )) 
+                    : ''}
+                </ul>         
+            </li>        
+        )}
+    </ul>
+  </>
+
+)
+
+export default Skilses;
