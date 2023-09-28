b5f4434000c911ac0c82fb1084a2382c8959c98e
grand3680 <kirill.burtsev07@gmail.com>
2023-09-29 01:05:26 +0300

    about page

--- src/components/aboutPage/About.js
+++ src/components/aboutPage/About.js
@@ -1,14 +1,42 @@
-import styles from './styles/about.module.scss'
-import Image from 'next/image'
-import profilePic from '@/images/rudeface.png'
+import styles from './styles/about.module.scss';
+import Image from 'next/image';
+import profilePic from '@/images/rudeface.png';
+import { useRef, useLayoutEffect} from 'react';
+import TypingText from './typing';
 
 const about = () => {
+    const aboutRef = useRef(null);
+
+    useLayoutEffect(() => {
+        const AboutElement = aboutRef.current;
+
+        if (AboutElement) {
+            new TypingText (
+                AboutElement,
+                {
+                    textContent : [
+                        'hey',
+                        'I"m proggrammer',
+                        'I know - ',
+                        'HTML/css',
+                        'JS ( THREE.JS, jquery )',
+                        'SCSS and SASS  ❤️ ( I used on this project )'
+                    ],
+                    id : 1,
+                    delay : 125,
+                    backDelay : 25,
+                }
+            );
+
+        }
+    }, []);
+
     return ( 
     <> 
         <section className={styles.information}>
             <div className={styles.section_information}>
                 <div className={styles.content_information}>
-                    <h1 className={styles.title_information}>About me</h1>    
+                    <h1 ref={aboutRef} className={styles.title_information}>About me</h1>    
                     <span className={styles.about_text}>
                         <p>Hey there,</p>
                         <p>My name's Kirill. I'm a programmer focused on graphics, animation, and interaction for web experiences and games.</p>
---
+++ src/components/aboutPage/typing.js
@@ -0,0 +1,72 @@
+class TypingText {
+    constructor(selectEl, options) {
+      this.options = options;
+      this.content = this.options.textContent;
+      this.el$ = selectEl;
+      this.textArr = this.content;
+      this.backAnimate = false;
+  
+      this.indexLetter = 0;
+      this.indexWord = 0;
+  
+      this.currentId = this.getValueOrDefault("id", this.options, 1);
+      this.delay = this.getValueOrDefault("delay", this.options, 100);
+      this.backDelay = this.getValueOrDefault("backDelay", this.options, this.delay);
+  
+      if (this.currentId !== null) {
+        this.typingAnimate();
+      }
+    }
+  
+    getValueOrDefault(name, option, defaultValue = null) {
+      return typeof option[name] !== "undefined" ? option[name] : defaultValue;
+    }
+  
+    typingAnimate() {
+      this.animateTexting();
+  
+      setTimeout(() => {
+        this.typingAnimate();
+      }, this.backAnimate ? this.backDelay : this.delay);
+    }
+  
+    setActiveWord() {
+      const arrLen = this.textArr.length;
+      return this.indexWord + 1 < arrLen ? (this.indexWord += 1) : (this.indexWord = 0);
+    }
+  
+    animateText() {
+      this.el$.innerHTML = "";
+  
+      for (let i = 0; i < this.indexLetter; i++) {
+        const word = this.textArr[this.indexWord];
+        this.el$.innerHTML += word[i];
+      }
+    }
+  
+    animateTexting() {
+      if (this.indexLetter > this.textArr[this.indexWord].length) {
+        this.backAnimate = true;
+      }
+  
+      switch (this.backAnimate) {
+        case true:
+          this.indexLetter -= 1;
+          this.animateText();
+          break;
+  
+        case false:
+          this.animateText();
+          this.indexLetter += 1;
+          break;
+      }
+  
+      if (this.indexLetter <= 0) {
+        this.backAnimate = false;
+        this.indexLetter = 0;
+        this.setActiveWord();
+      }
+    }
+  }
+  
+  export default TypingText;
