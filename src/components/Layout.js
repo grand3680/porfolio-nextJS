8826c9b11c5f9f4e7827bd1cdab7cf0a160b4a53
grand3680 <kirill.burtsev07@gmail.com>
2023-09-29 19:55:25 +0300

    try make fonts

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,20 @@
 import Heading from '@/components/Heder/Heding';
 import container from '@/styles/container.module.scss';
 
-// import aboutPage from '@/components/about/';
+import { Roboto } from 'next/font/google'
+ 
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
+ 
 
 const Layout = ({ children }) => {
     return (
         <>
             <Heading/>
 
-            <div className={container.container}> 
+            <div className={`${container.container} ${roboto.className}`}> 
                 {children}
             </div>
 
