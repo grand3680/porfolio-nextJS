5a59ed475d621419d011a092d556207b5d269d34
grand3680 <kirill.burtsev07@gmail.com>
2023-09-30 19:37:51 +0300

    Fonts

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -1,12 +1,18 @@
-import styles from './styles/home.module.scss'
+import styles from './styles/home.module.scss';
+import { MinecraftFont } from '@/fonts';
 
-const home = () => {
+import { FC } from 'react';
+ 
+// const fot = font;
+// Font files can be colocated inside of `pages`
+
+const home : FC = () => {
     return ( 
     <> 
         <section className={styles.HomePg}>
             <div className={styles.HomePage_info}>
                 <div className={styles.HomePage_info_content}>
-                    <h1 className={styles.title_content}>Hello! I'm a <span>programmer</span></h1>      
+                    <h1 className={styles.title_content}>Hello! I'm a <span className={MinecraftFont.className}>programmer</span></h1>      
                     <h2 className={styles.Subtitle_content}>So this my webSite on NextJS</h2>
                     <p className={styles.text_content}>texting</p>
                 </div>
---
+++ src/styles/fonts/fonts.ts
@@ -0,0 +1,8 @@
+import localFont from 'next/font/local'
+
+const MinecraftFont = localFont({ src: './MinecraftFont.ttf' })
+const SintonyBold = localFont({ src: './Sintony-Bold.ttf' })
+const SintonyRegular = localFont({ src: './Sintony-Regular.ttf' })
+ 
+ 
+export { SintonyRegular, SintonyBold, MinecraftFont }
